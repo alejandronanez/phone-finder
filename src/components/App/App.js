edcf52707297d254f7160114245f542ce3c04456
Alejandro Nanez <alejonanez@gmail.com>
2018-12-07 08:50:38 -0500

    WIP

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,30 @@ import 'components/App/App.scss';
 
 class App extends Component {
   render() {
-    return <div className="App">Phone Finder</div>;
+    return (
+      <main className="App">
+        <h1 className="App__title">Phone number checker</h1>
+        <h2 className="App__sub-title">
+          All the information you need about any phone number, just one click
+          away
+        </h2>
+        <form
+          className="Form"
+          onSubmit={e => {
+            e.preventDefault();
+            console.log('Submitting form');
+          }}
+        >
+          <input
+            className="Input"
+            type="text"
+            placeholder="Type your number"
+            required
+          />
+          <button className="Button">Find phone information</button>
+        </form>
+      </main>
+    );
   }
 }
 
