000355d957cfdb878cd75ea7f33ca23820c9d37e
Alejandro Nanez <alejonanez@gmail.com>
2018-12-08 11:11:07 -0500

    Start of exercise two. - Markup ready.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,40 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return <div className="App">Phone Finder</div>;
+  return (
+    <main className="App">
+      <h1 className="App__title">Phone number checker</h1>
+      <h2 className="App__sub-title">
+        All the information you need about any phone number, just one click away
+      </h2>
+      <form className="Form">
+        <input
+          className="Input"
+          type="text"
+          placeholder="Type your number"
+          required
+        />
+        <button className="Button">Find phone information</button>
+      </form>
+      <ul className="Result">
+        <li>
+          <strong>Valid: </strong>
+          <span role="img" aria-label="Phone valid">
+            ✅
+          </span>
+        </li>
+        <li>
+          <strong>International format: </strong>
+        </li>
+        <li>
+          <strong>Country: </strong>
+        </li>
+        <li>
+          <strong>Location: </strong>
+        </li>
+      </ul>
+    </main>
+  );
 }
 
 export default App;
